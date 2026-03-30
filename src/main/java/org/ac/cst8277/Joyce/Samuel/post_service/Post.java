006833c17d5338ed0070b596978f0a1fbe17a5f1
Sam Joyce <samdjoyce@gmail.com>
2026-03-30 14:55:17 -0400

    complete

--- src/main/java/org/ac/cst8277/Joyce/Samuel/post_service/Post.java
+++ src/main/java/org/ac/cst8277/Joyce/Samuel/post_service/Post.java
@@ -74,6 +74,8 @@ public class Post {
 
 	public void setTimestamp(LocalDateTime timestamp) {
 		this.timestamp = timestamp;
-		this.raw_time = Timestamp.valueOf(timestamp);
+		if (timestamp != null) {
+			this.raw_time = Timestamp.valueOf(timestamp);
+		}
 	}
 }
